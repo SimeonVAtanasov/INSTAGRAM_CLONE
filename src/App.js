b518914a484565bb097eb73ce7d57bdbade31738
Simeon Atanasov <simoatansov76@gmail.com>
2021-03-19 00:00:08 +0200

    Basic router implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header>
+        
+          <nav>
+            <ul className="mainNav">
+              <li id="logo">
+                <Link to="/">
+                  <img src={"logoHeader.png"} alt="instagram" />
+                </Link>
+              </li>
+              <li id="inputMain">
+                <form>
+                  <input />
+                </form>
+              </li>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/inbox">Inbox</Link>
+              </li>
+              <li>
+                <Link to="/explore">Explore</Link>
+              </li>
+              <li>
+                <Link to="/notifications">Notifications</Link>
+              </li>
+              <li>
+                <Link to="profile">Profile</Link>
+              </li>
+            </ul>
+          </nav>
       </header>
-    </div>
+
+      <Switch>
+        <Route path="/inbox">
+          <Inbox />
+        </Route>
+        <Route path="/explore">
+          <Explore />
+        </Route>
+        <Route path="/notifications">
+          <Notifications />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
+function Home() {
+  return (
+    <h2>Home</h2>
+  );
+}
+
+function Inbox() {
+  return (
+    <h2>Inbox</h2>
+  );
+}
+
+function Explore() {
+  return (
+    <h2>Explore</h2>
+  );
+}
+
+function Notifications() {
+  return <h2>Notifications</h2>;
+}
+
+function Profile() {
+  return <h2>Profile</h2>;
+}
 export default App;
