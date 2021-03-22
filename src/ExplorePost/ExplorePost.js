97e795d622b0c51c44d51e0640224b501b0e060b
Simeon Atanasov <simoatansov76@gmail.com>
2021-03-22 22:19:15 +0200

    Added adtitional fields in the firebase posts  collection

--- src/ExplorePost/ExplorePost.js
+++ src/ExplorePost/ExplorePost.js
@@ -1,24 +1,24 @@
 import ImgExplore from "../ImgExplore/ImgExplore";
-import  styles  from  "./ExplorePost.module.css";
+import styles from "./ExplorePost.module.css";
 import ChatBubbleOutlineOutlinedIcon from '@material-ui/icons/ChatBubbleOutlineOutlined';
 import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
-export default function ExplorePost({imageUrl, caption}) {
-     
+export default function ExplorePost({ imageUrl, caption, comments, likes }) {
+
 
 
     return (
         <>
-        <div  className={styles.postLayout}>
-            {/* <a> */}
-            <div className={styles.imagebox}>
-                <ImgExplore src={imageUrl} alt={caption}/>
+            <div className={styles.postLayout}>
+                {/* <a> */}
+                <div className={styles.imagebox}>
+                    <ImgExplore src={imageUrl} alt={caption} />
+                </div>
+                <div className={styles.aditives}>
+                    <p><FavoriteBorderOutlinedIcon />{(likes) ? likes : <></>}</p>
+                    <p><ChatBubbleOutlineOutlinedIcon />{(comments) ? comments.length : <></>}</p>
+                </div>
+                {/* <a/> */}
             </div>
-            <div className={styles.aditives}>
-                <p><FavoriteBorderOutlinedIcon/></p>
-                <p><ChatBubbleOutlineOutlinedIcon/></p>
-            </div>
-            {/* <a/> */}
-        </div>
-    </>
+        </>
     );
-  }
+}
