fcfbbd3cbd2532fda67ecbe97a4a06bb5bf23d3a
Simeon Atanasov <simoatansov76@gmail.com>
2021-03-30 00:43:54 +0300

    Simple user search implemented in chatroom

--- src/ChatRoom/ChatRoom.js
+++ src/ChatRoom/ChatRoom.js
@@ -4,12 +4,13 @@ import { v4 } from 'uuid';
 import { db } from '../firebase';
 import stylesB from "./ChatRoom.module.scss"
 import MessageForm from "./MessageForm/MessageForm"
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 export default function ChatRoom() {
     const [converstaions, setConversations] = useState([]);
     const [convoId, setConvoId] = useState('');
     const [users, setUsers] = useState([]);
-    const [searchUsersInput, setSearchUsersInput] = useState("");
+    const [searchInput, setSearchInput] = useState("");
     useEffect(() => {
         db.collection("chatRooms")
             .where("users", "array-contains", "yI7uZCourOVAqWt1EIFTEJlrnim1")
@@ -20,8 +21,26 @@ export default function ChatRoom() {
 
             })
     }, [])
-    useEffect(() => { }, [users])
 
+
+    const handleInput = (ev) => {
+        ev.preventDefault();
+        let text = ev.target.value;
+        if(text){
+            db.collection("users")
+            .get()
+            .then((querySnapshot) => {
+                let fetchedUsers = [];
+                querySnapshot.forEach((doc) => {
+                    fetchedUsers.push(doc.data());
+                });
+                let filteredUsers = fetchedUsers.filter(user => user.displayName.toLowerCase().split(' ').join("").includes(text))
+                setUsers(filteredUsers)
+            })
+        }
+        
+            
+    }
     return (
         <section className={stylesB.chatRoom}>
             <div className={stylesB.convoBox}>
@@ -30,13 +49,13 @@ export default function ChatRoom() {
                     {/* <TextField className={stylesB.searchInput} id="outlined-basic" label="До: " variant="outlined" />
                     <Autocomplete /> */}
 
-                    {/* <Autocomplete
+                    <Autocomplete
                         id="searchUsers"
                         options={users}
-                        getOptionLabel={(user) => user.displayname}
+                        getOptionLabel={(user) => user.displayName}
                         style={{ width: 300 }}
-                        renderInput={(params) => <TextField {...params} label="До:" variant="outlined" />}
-                    /> */}
+                        renderInput={(params) => <TextField {...params} onChange={(ev) => { handleInput(ev) }} label="До:" variant="outlined" />}
+                    />
                 </form>
                 <div className={stylesB.convoBox}>
                     {converstaions.map(convo => <h3 key={v4()} onClick={() => { setConvoId(convo.convoId) }}>{convo.convoId}</h3>)}
